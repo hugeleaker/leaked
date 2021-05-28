68888a15185b7faa30ac473f85ce288e297ecd6e
hugeleaker <84783974+hugeleaker@users.noreply.github.com>
2021-05-28 04:55:55 -0700

    Create webpage.cpp

---
+++ webpage.cpp
@@ -0,0 +1,9 @@
+# This should be detected
+password = "testing_new_pwd";
+password: "lol";
+pwd: "new_db"
+set_pwd("Password_077");
+passwordField.setValue("Ex@mplePassw0rd");
+account(user="admin", password="test123");
+account(user="admin", password="test123").connect_server();
+pwd = "erg!e13@ee.f";
